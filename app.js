fe95da9e7eb9f8b4b76cc48eb8d829c30c37fe1b
Benjamin Comeau <bcomeau@gwmail.gwu.edu>
2016-12-30 13:35:32 -0500

    win conditions are getting screwy

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
 //variables
-var win = false;
-var winner = {
- X: "X Wins!",
- O: "O Wins!",
- wins: [7,56,448,73,146,292,273,84]
+
+var tictactoe = [ null, null, null, null, null, null, null, null, null ];
+var score = {
+  'X': [ null, null, null, null, null, null, null, null, null ],
+  'O': [ null, null, null, null, null, null, null, null, null ]
+};
+
+var turn = 'X';
+
+// <td data-index="0"></td>
+// <td data-index="1"></td>
+
+var sets = [
+  // ROWS
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  // COLUMNS
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  // DIAGONALS
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
+function checkwinners(){
+  var candidate = score[turn];
+  var condition = [turn, turn, turn];
+  var game_over = false;
+  sets.forEach(function(set){
+    var line = [
+    score[turn][set[0]],
+    score[turn][set[1]],
+    score[turn][set[1]]
+    ];
+    console.log(line, condition);
+
+    if (_.isEqual(line, condition)) {game_over=true;}
+  });
+
+  // _ is using underscore library to access underscore's functions to compare canidadate with conditions to win
+
+  return game_over;
 }
+
+
+
+function changeturn(){
+   if (turn === 'X') { turn = 'O'; }
+   else { turn = 'X'; }
+}
+
 
 
 //on click the game alternates between "X" and "O"
@@ -17,25 +64,26 @@ var game = function(turn) {
             $(this).addClass('O');
         }
         turn++;
-    //shakes each icon as its placed,looks kind of ugly...maybe play a sound instead
-        // $(this).effect('shake', 10);
     });
 }
 
-//effect to shake the board when a victory occurs and win===true
-
-var winShake = function(win){
-	while(win===true){
-		$('.ticTable').effect('shake');
-		alert(winner);
-	}
-	}
 
 //code begin
 
 $(document).ready(function() {
-    game(turn);
-    winShake();
+  $('.ticTable').on('click', '.cell', function(){
+    var $cell = $(this);
+    var index = $cell.data('index');
+    $cell.addClass(turn);
+    score[turn][index] = turn;
+
+    if (checkwinners()){
+      $('.winner').text(turn + " won!")
+      $('.results').show();
+    };
+    changeturn();
+  });
+  });
     // winCheck();
 
 
@@ -51,49 +99,6 @@ $(document).ready(function() {
 
 //game reload
 
-    $('.newGame').click(function() {
+    $('.replay, .newGame').click(function() {
         location.reload(true);
     });
-
-});
-
-
-// var winCheck = function(){
-
-
-// var tLeft = '.top td:nth-of-type(1)';
-// var tMiddle = '.top td:nth-of-type(2)';
-// var tRight = '.top td:nth-of-type(3)';
-
-// var cLeft = '.center td:nth-of-type(1)';
-// var cMiddle = '.center td:nth-of-type(2)';
-// var cRight = '.center td:nth-of-type(3)';
-
-// var bLeft = '.bottom td:nth-of-type(1)';
-// var bMiddle = '.bottom td:nth-of-type(2)';
-// var bRight = '.bottom td:nth-of-type(3)';
-
-// var tLeft_O = $(tLeft).hasClass('O');
-// var tMiddle_O = $(tMiddle).hasClass('O');
-// var tRight_O = $(tRight).hasClass('O');
-
-// var cLeft_O = $(cLeft).hasClass('O');
-// var cMiddle_O = $(cMiddle).hasClass('O');
-// var cRight_O = $(cRight).hasClass('O');
-
-// var bLeft_O = $(bLeft).hasClass('O');
-// var bMiddle_O = $(bMiddle).hasClass('O');
-// var bRight_O = $(bRight).hasClass('O');
-
-// var tLeft_X = $(tLeft).hasClass('X');
-// var tMiddle_X = $(tMiddle).hasClass('X');
-// var tRight_X = $(tRight).hasClass('X');
-
-// var cLeft_X = $(cLeft).hasClass('X');
-// var cMiddle_X = $(cMiddle).hasClass('X');
-// var cRight_X = $(cRight).hasClass('X');
-
-// var bLeft_X = $(bLeft).hasClass('X');
-// var bMiddle_X = $(bMiddle).hasClass('X');
-// var bRight_X = $(bRight).hasClass('X');
-
